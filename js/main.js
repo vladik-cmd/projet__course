151740c7bbadf5cf3807ec12570306936a882e32
Vladislav <trushovvladislav@gmail.com>
2020-10-17 12:33:10 +0300

    Практика 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 "use strict";
 
-const isChecked = false,
-      isClose = true;
+const numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(isChecked || !isClose);
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const lastWatchFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingOfFilm1 = prompt('На сколько оцените его?', ''),
+      lastWatchFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingOfFilm2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWatchFilm1] = ratingOfFilm1;
+personalMovieDB.movies[lastWatchFilm2] = ratingOfFilm2;
+
+console.log(personalMovieDB);
