78c9a92ab17c8798ad01f716cd0ff0467d803ec9
Vladik <trushovvladislav@gmail.com>
2021-09-16 23:04:09 +0300

    modification of the output of cards

--- js/script.js
+++ js/script.js
@@ -183,37 +183,29 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  new MenuItem(
-    'img/tabs/vegy.jpg',
-    'vegy',
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    '.menu .container',
-    'menu__item',
-  ).render();
+  const getResourse = async (url) => {
+    const res = await fetch(url);
 
-  new MenuItem(
-    'img/tabs/elite.jpg',
-    'elite',
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    '.menu .container',
-    'menu__item',
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
 
-  new MenuItem(
-    'img/tabs/post.jpg',
-    'post',
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    14,
-    '.menu .container',
-    'menu__item',
-  ).render();
+    return await res.json();
+  };
 
-  //FORMS
+  // getResourse('http://localhost:3000/menu').then((data) => {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     new MenuItem(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
+  //   });
+  // });
+
+  axios.get('http://localhost:3000/menu').then(data => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuItem(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
+    });
+  });
+
+  //FORMSç
 
   const forms = document.querySelectorAll('form');
 
@@ -224,10 +216,22 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: data,
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -241,20 +245,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
       const formData = new FormData(form);
 
-      const obj = {};
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      formData.forEach(function (value, key) {
-        obj[key] = value;
-      });
-
-      fetch('server.php', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(obj),
-      })
-        .then((data) => data.text())
+      postData('http://localhost:3000/requests', json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.succses);
@@ -292,5 +285,4 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
-  
 });
